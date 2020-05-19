c1178b66d100b1eaae94d63fd67477e7f0866e55
ManhTruong <manhtruongbui63@gmail.com>
2020-05-19 11:30:19 +0700

    create data

--- public/js/app.js
+++ public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\taoph\Desktop\Demo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\taoph\Desktop\Demo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\truong.buimanh\Desktop\Demo\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\truong.buimanh\Desktop\Demo\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
